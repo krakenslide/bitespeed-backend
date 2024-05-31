b0cdf44840f5947cca21667047e9b8c67a5aeefd
Ankit Mukhopadhyay <muk.ankit@gmail.com>
2024-05-31 20:53:45 +0530

    feat: add swagger doc

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import identifyRoutes from "./routes/routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
 const app = express();
-app.use(express.json());
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "BiteSpeed Backend",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./src/routes/routes.ts"],
+};
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
 app.use("/api", identifyRoutes);
 
 const PORT = process.env.PORT || 3000;
